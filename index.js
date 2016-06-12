f97d0819b5809fc44ea6fcc1e3db3d708d76fdaa
somax <somaxj@163.com>
2016-06-12 21:09:28 +0800

    update ecosystem.json

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.argv[2] || process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
       res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
-          res.write('COMMON_VARIABLE: '+ process.env.COMMON_VARIABLE);
+          res.write('DEBUG: '+ process.env.DEBUG);
           res.write('\nNODE_ENV: '+ process.env.NODE_ENV);
           res.end('\nHello PM2\n');
 });
